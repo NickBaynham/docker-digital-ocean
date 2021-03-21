cb4834c53922986d390b0d0c8f8a584dd21824e6
Nick Baynham <nickbaynham@gmail.com>
2021-03-21 07:04:49 -0400

    Added quotes to variable names

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,27 +1,27 @@
-variable do_token {
+variable "do_token" {
   description = "Token for DigitalOcean Authentication"
   type = string
 }
 
-variable droplet_name {
+variable "droplet_name" {
   description = "Name of the Droplet to Create"
   type = string
   default = "Ubuntu-2"
 }
 
-variable image_name {
+variable "image_name" {
   description = "The name of the droplet image to use"
   type = string
   default = "ubuntu-20-04-x64"
 }
 
-variable do_region {
+variable "do_region" {
   description = "The region to create the droplet"
   type = string
   default = "nyc1"
 }
 
-variable droplet_size {
+variable "droplet_size" {
   description = "The size of the droplet to create"
   type = string
   default = "512mb"
